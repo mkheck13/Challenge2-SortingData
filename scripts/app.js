bf0c4d6bf4d36ba1d89c7b6830753a42b5954921
mkheck13 <mkheck13@gmail.com>
2024-02-14 11:14:33 -0800

    Fetching data

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+import personData from "../data/data.json" assert {type:'json'}
+console.log(personData);
+
+// let but10;
+// let but20;
+// let but30;
+// let but40;
+// let but50;
+
+// let butId;
+// let butFirst;
+// let butLast;
+// let butHeight;
+// let butAge;
+
